bf0e042254666e25a656582c04d6c7bf699c0860
Jean-Christophe Hoelt <hoelt@fovea.cc>
2015-06-14 09:08:17 +0000

    Fix redis_auth

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 cp /twemproxy.yml.in /twemproxy.yml
 LINKS="$(env|grep '_TCP='|cut -d/ -f3|sort|uniq)"
 index=1
-if [[ -z $REDIS_AUTH ]]; then
+if [[ -n $REDIS_AUTH ]]; then
 	echo "  redis_auth: $REDIS_AUTH" >> /twemproxy.yml
 fi
 echo "  servers:" >> /twemproxy.yml
